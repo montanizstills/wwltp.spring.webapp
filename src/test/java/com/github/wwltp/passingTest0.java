4b29e9ad1f5113a37f0d2040e517fef32318a4f2
Montaniz Stills <57910061+montanizstills@users.noreply.github.com>
2020-06-27 15:24:38 -0400

    updated `pom.xml`; created template test; updated `travis.yml`

---
+++ src/test/java/com/github/wwltp/passingTest0.java
@@ -0,0 +1,12 @@
+package com.github.wwltp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class passingTest0 {
+@Test
+    public void mytest(){
+    Assert.assertEquals(0,0);
+}
+}
