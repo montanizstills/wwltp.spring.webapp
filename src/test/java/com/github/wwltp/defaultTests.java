97827a32dd7e9229a63a295c612d110a5bcbf7b8
Montaniz Stills <57910061+montanizstills@users.noreply.github.com>
2020-07-02 11:47:24 -0400

    created `defaultTests`

---
+++ src/test/java/com/github/wwltp/defaultTests.java
@@ -0,0 +1,17 @@
+package com.github.wwltp;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class defaultTests {
+@Test
+    public void purposefulFail(){
+    Assert.assertTrue(false);
+
+}
+@Test
+    public void purposefulPass(){
+    Assert.assertTrue(true);
+}
+}
