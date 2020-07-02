30d9445cf4959af7113d7dad6883cf3a108d7031
Montaniz Stills <57910061+montanizstills@users.noreply.github.com>
2020-07-02 11:53:07 -0400

    updated `defaultTest.java`

--- src/test/java/com/github/wwltp/defaultTests.java
+++ src/test/java/com/github/wwltp/defaultTests.java
@@ -5,11 +5,11 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class defaultTests {
-@Test
-    public void purposefulFail(){
-    Assert.assertTrue(false);
-
-}
+//@Test
+//    public void purposefulFail(){
+//    Assert.assertTrue(false);
+//
+//}
 @Test
     public void purposefulPass(){
     Assert.assertTrue(true);
