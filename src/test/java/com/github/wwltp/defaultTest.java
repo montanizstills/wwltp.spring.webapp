3b0beea855fe4b9a05a275d761c378f23bb8aa2c
Montaniz Stills <57910061+montanizstills@users.noreply.github.com>
2020-07-05 17:12:13 -0400

    updated pom.xml; renamed defaultTests -> defaultTest

--- src/test/java/com/github/wwltp/defaultTest.java
+++ src/test/java/com/github/wwltp/defaultTest.java
@@ -4,11 +4,10 @@ package com.github.wwltp;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class defaultTests {
+public class defaultTest {
     @Test
     public void purposefulFail() {
         Assert.assertTrue(false);
-
     }
 //@Test
 //    public void purposefulPass(){
